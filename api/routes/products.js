880ab0cfa60710218ad8a4ffd3134cf6a799cd1b
Olga <ipvola@gmail.com>
2020-09-12 23:25:39 -0400

    custom response witt url

--- api/routes/products.js
+++ api/routes/products.js
@@ -11,10 +11,31 @@ router.get('/', (req, res, next) => {
 
     // this Model object 'Product' has also: where().limit()
     Product.find() // without param 'find' will return arr of all docs
+        .select('name price _id')// only those fields will be fetched from db doc, i do not want "__v"
         .exec()
         .then(docs => {
-            console.log(docs);
-            res.status(200).json(docs);
+            const response = {
+                count : docs.length,
+                arr_of_products: docs.map( doc => {
+                    return {
+                        name  : doc.name,
+                        price : doc.price,
+                        _id   : doc._id,
+                        you_can_send_request : {
+                            type : 'GET',
+                            url  : 'Dynamically fetch to get the id of the service i am running on: http://localhost:3000/products/' + doc._id
+                        }
+                    };
+                })
+            };
+            // [] will be returned if no records
+            if(docs.length > 0){
+                res.status(200).json(response);
+            }else{
+                res.status(404).json({
+                    message: 'no entries any more'
+                });
+            }
         })
         .catch(err => {
             console.log(err);
@@ -43,11 +64,9 @@ router.post('/', (req, res, next) => {
     product.save() // storing in DB
         .then(result => {
             res.status(201).json({
-                message: 'Handling POST requests to /products',
+                message: 'New product has been created!',
                 my_product: product
             });
-            console.log('***********************************---***************');
-            console.log(result);
         })
         .catch(err => {
             console.log(err);
@@ -92,9 +111,31 @@ router.get('/:anyVarNameID', (req, res, next) => {
 });
 
 router.patch('/:anyVarNameID', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated product!'
-    });
+    // res.status(200).json({
+    //     message: 'Updated product!'
+    // });
+    const id = req.params.anyVarNameID;
+
+    // we should adjust to any req: with payload as newName only, or both, or with NO payload
+    const updatesOperations = {};
+    for( const ops of req.body) {// req.body will be an arr  with operations. propName & value belong to operations
+        updatesOperations[ops.propName] = ops.value;
+    }
+    // $set is a special propert name understood by mongoose
+    // Product.update({_id : id}, {$set: { name : req.body.newName, price: req.body.newPrice }});
+    Product.update({_id : id}, {$set: updatesOperations})
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result); 
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                my_message: 'Mongoose error when trying to Update',
+                my_error:  err
+            });
+        });
 });
 
 router.delete('/:anyVarNameID', (req, res, next) => {
@@ -103,14 +144,10 @@ router.delete('/:anyVarNameID', (req, res, next) => {
     // });
 
     // Get id from the URL
-    const id = req.body.anyVarNameID;
-    console.log('***************');
-    console.log(id);
-    console.log('***************');
-    
-
+    const id = req.params.anyVarNameID;
+     
     // object inside should have 1 or more criteria of object to remove
-    Product.remove({name : 'Dary Hoff'})
+    Product.deleteOne({_id : id})
         .exec()
         .then(result => {
             res.status(200).json(result);
--- models/productSchema.js
+++ models/productSchema.js
@@ -2,9 +2,15 @@ const mongoose = require('mongoose');
 
 // param inside is a schema how my product should look like
 const productSchema = mongoose.Schema({
-  _id: mongoose.Schema.Types.ObjectId, // value here defines a type of data:long string as a serial number
-  name : String,
-  price: Number
+    _id: mongoose.Schema.Types.ObjectId, // value here defines a type of data:long string as a serial number
+    name : {
+        type: String,
+        required : true
+    },
+    price: {
+        type: Number,
+        required : true
+    }
 });
 
 /* now we create a real js object=model(created by given layout) which will have CRUD methods
